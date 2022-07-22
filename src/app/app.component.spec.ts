6a1b97a76d79f35cc055163925446d7cf0a3d26e
marianamaher <mariana.maherr@gmail.com>
2022-07-22 10:51:25 -0400

    changed design and main content

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hello-world'`, () => {
+  it(`should have as title 'tempo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('hello-world');
