6ee9f553a904fab2eeb980a513ec65019ba66ecc
marianamaher <mariana.maherr@gmail.com>
2022-07-22 12:22:37 -0400

    updating body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PomodoroComponent } from './pomodoro/pomodoro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PomodoroComponent
   ],
   imports: [
     BrowserModule
