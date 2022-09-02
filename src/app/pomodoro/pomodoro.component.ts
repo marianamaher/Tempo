8db8ee2376c338c60531f8d88248e9ccf6f932f4
marianamaher <mariana.maherr@gmail.com>
2022-09-02 16:38:39 -0400

    adjusted timer, added entry box

--- src/app/pomodoro/pomodoro.component.ts
+++ src/app/pomodoro/pomodoro.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PomodoroComponent implements OnInit {
 
-  constructor() { }
+  workDuration= 25
+  minutes = this.workDuration-1
+  seconds = 60
+  interval:any = 0
+  message = 'Get to work!'
+
+
+  constructor() {
+ 
+   }
 
   ngOnInit(): void {
+  }
+  
+  start(){
+
+    this.interval = setInterval(()=> counter(), 1000)
+
+    const counter= () =>{
+      this.seconds--
+      if(this.seconds==-1){
+        this.minutes--
+        if(this.minutes==-1){
+          this.message='Now get some deserved rest!'
+        }
+      }
+    }
   }
 
 }
